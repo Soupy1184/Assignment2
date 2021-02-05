8e49735a22f18d203130e85848f9c6f5eb2e34c2
christophercampbell <chris.campbell1184@gmail.com>
2021-02-05 00:00:22 -0500

    v1

--- Main.java
+++ Main.java
@@ -1,5 +1,27 @@
 public class Main {
     public static void main(String[] args){
-        System.out.println("Assignment #2");
+        String plaintext = "Hello world, what a beautiful day (today?).";
+        alphabetToIntArray(plaintext);
+
+
+    }
+
+    public static int[] alphabetToIntArray(String string){
+        string = string.toLowerCase();
+        char[] stringArray = string.toCharArray();
+        String alphabet = "abcdefghijklmnopqrstuvwxyz .,?()";
+        char[] aplhaArray = alphabet.toCharArray();
+        int[] zSpace = new int[stringArray.length];
+        
+        for(int i = 0; i < stringArray.length; i++){
+            for (int j = 0; j < aplhaArray.length; j++){
+                if (stringArray[i] == aplhaArray[j]){
+                    zSpace[i] = j;
+                    System.out.printf("%d, ", zSpace[i]);
+                }
+            }
+        }
+
+        return zSpace;
     }
 }
